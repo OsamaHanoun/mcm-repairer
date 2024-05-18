406bc17e0c514eb61c44a8be7423a815047563ad
OsamaHanoun <osamahanoun997@gmail.com>
2024-05-19 01:42:55 +0200

    style: format file

--- mcm_repairer/file_io.py
+++ mcm_repairer/file_io.py
@@ -2,14 +2,16 @@ from trimesh import Geometry, load_mesh
 from os.path import isfile
 from OCC.Core.StlAPI import StlAPI_Writer
 
-#TODO: replace trimesh with OCC.Core.RWStl in order to remove trimesh and scipy. OCC.Core.RWStl does not split a mesh so a mesh splitter method needs no be implemented. 
+# TODO: replace trimesh with OCC.Core.RWStl in order to remove trimesh and scipy. OCC.Core.RWStl does not split a mesh so a mesh splitter method needs no be implemented.
 
-def load_stl(file_path:str)-> list[Geometry]:
+
+def load_stl(file_path: str) -> list[Geometry]:
     if not isfile(file_path):
         raise ValueError(f"File does not exist: {file_path}")
-    mesh: Geometry = load_mesh(file_path,'stl')
-    meshes: list[Geometry]= mesh.split(only_watertight=True)
+    mesh: Geometry = load_mesh(file_path, "stl")
+    meshes: list[Geometry] = mesh.split(only_watertight=True)
     return meshes
+
 
 def export_to_stl(shape, filename, ascii_mode=False):
     writer = StlAPI_Writer()
